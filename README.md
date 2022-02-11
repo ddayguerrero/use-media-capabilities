ace7b760f719798e45830d7794a012c0c7437517
ddayguerrero <ddayy.guerrero@hotmail.com>
2022-02-11 00:58:12 -0500

    tweak configuration

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # use-media-capabilities
+
+React Hook to extract information about the decoding and encoding capabilities of a given audio or video format.
--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,28 @@
-export default {
-  input: "./out-tsc/src/index.js",
-  output: {
-    file: "dist/index.js",
-    format: "es",
+const packageJson = require("./package.json");
+import dts from "rollup-plugin-dts";
+
+export default [
+  {
+    input: "./out-tsc/src/index.js",
+    output: [
+      {
+        file: packageJson.module,
+        format: "es",
+      },
+      {
+        file: packageJson.main,
+        format: "cjs",
+        name: "use-media-capabilities",
+      },
+    ],
+    external: [
+      'react',
+      'react-dom',
+    ],
   },
-  external: ["react"],
-};
+  {
+    input: "out-tsc/types/src/index.d.ts",
+    output: [{ file: "dist/index.d.ts", format: "esm" }],
+    plugins: [dts()],
+  },
+];
